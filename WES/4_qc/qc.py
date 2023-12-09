b11cc4d197bc21aac8142498a570ae59cf0f75d5
Patrick Basitta <pbasitta@ukb2580.klinik.bn>
2023-12-09 13:58:53 +0100

    qc

--- WES/4_qc/qc.py
+++ WES/4_qc/qc.py
@@ -12,6 +12,7 @@ import os
 import re
 import argparse
 import json
+import itertools
 
 # using argparse for positinal arguments
 parser = argparse.ArgumentParser()
@@ -19,23 +20,36 @@ parser.add_argument("-sf", "--qc_folder", type=str)
 parser.add_argument("-o", "--outdir", type=str)            
 args = parser.parse_args()
 
-directory = args.qc_folder
-
+# directory = args.qc_folder
+directory =  ""
 # get qc data
 qc_files = os.listdir(directory)
 
 # make dixt to get case idx
-idx_qc_files_dict = {}
+idx_qc_files_dict_normal = {}
+idx_qc_files_dict_tumor = {}
 for idx, file in enumerate(qc_files):
     case_no = int(file.split("_")[0])
-    idx_qc_files_dict[case_no] = idx
-
+    prefix = file.split("_")[2]
+    if prefix == "tumor":
+        idx_qc_files_dict_tumor[case_no] = idx
+    if prefix == "normal":
+        idx_qc_files_dict_normal[case_no] = idx
+        
 # sort case idx dict
-sorted_idx_qc_files_dict = dict(sorted(idx_qc_files_dict.items()))
-    
-sorted_qc_files = []
-for i in list(sorted_idx_qc_files_dict.values()):
-    sorted_qc_files.append(qc_files[i])
+sorted_idx_qc_files_n_dict = dict(sorted(idx_qc_files_dict_normal.items()))
+sorted_idx_qc_files_t_dict = dict(sorted(idx_qc_files_dict_tumor.items())) 
+ 
+sorted_qc_n_files = []
+for i in list(sorted_idx_qc_files_n_dict.values()):
+    sorted_qc_n_files.append(qc_files[i])
+
+sorted_qc_t_files = []
+for i in list(sorted_idx_qc_files_t_dict.values()):
+    sorted_qc_t_files.append(qc_files[i])
+
+sorted_qc_files = [x for x in itertools.chain.from_iterable\
+       (itertools.zip_longest(sorted_qc_t_files,sorted_qc_n_files)) if x]
 
 # get qc data
 qc_data_dict = {}
@@ -45,7 +59,7 @@ for index, qc_file in enumerate(sorted_qc_files):
             case_num = qc_file.split("_")[0]
         if qc_file.split("_")[2] == "normal":
             case_num = qc_file.split("_")[0]+"-N"
-        QC_data = pd.read_csv(qc_file, delimiter= "\t", engine="python", header=None)
+        QC_data = pd.read_csv(directory+qc_file, delimiter= "\t", engine="python", header=None)
         QC_data = QC_data.T
         qc_data_dict[case_num] = list(QC_data.iloc[1])
         
@@ -71,4 +85,4 @@ final_qc_data = pd.DataFrame(qc_data_lst, columns= header_col)
 final_qc_data.insert(loc=0, column="Standort", value="Bonn")
 
 # write csv
-final_qc_data.to_csv(args.outdir+"qc.csv", index=False)
+final_qc_data.to_csv("qc.csv", index=False)
